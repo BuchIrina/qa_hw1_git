941b067bbfb96425d4bebe19dc2fc4040a8bcd97
Irina Buchneva <buchneva.irina986@gmail.com>
2022-09-16 11:20:00 +0300

    added second test and changed logic

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ExampleTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void ExampleTest() {
+        Assertions.assertTrue(3 >= 2);
+    }
+
+    void ExampleTest2() {
+        Assertions.assertTrue(4 >= 3);
+    }
 }
